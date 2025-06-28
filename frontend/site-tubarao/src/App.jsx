b6623c8d9fc4c6e985e39908df7fc0ee111a713b
fabriciobrito01 <fabriciodbe@gmail.com>
2025-06-28 19:36:38 -0300

    Add Carnaval pages at App.jsx

--- frontend/site-tubarao/src/App.jsx
+++ frontend/site-tubarao/src/App.jsx
@@ -2,6 +2,10 @@ import Home from './pages/Home.jsx';
 import InstitutoTubarao from './pages/InstitutoTubarao.jsx';
 import HistoriaTubarao from './pages/HistoriaTubarao.jsx';
 import FichaTecnica from './pages/FichaTecnica.jsx';
+import Carnaval2022 from './pages/Carnaval2022.jsx';
+import Carnaval2023 from './pages/Carnaval2023.jsx';
+import Carnaval2024 from './pages/Carnaval2024.jsx';
+import Carnaval2025 from './pages/Carnaval2025.jsx';
 import Navbar from './components/Navbar.jsx';
 import Footer from './components/Footer.jsx'; 
 import './index.css';
@@ -17,6 +21,10 @@ function App() {
           <Route path="/institutotubarao" element={<InstitutoTubarao />} />
           <Route path="/tubarao/historia" element={<HistoriaTubarao />} />
           <Route path="/tubarao/fichatecnica" element={<FichaTecnica />} />
+          <Route path="/tubarao/carnaval2022" element={<Carnaval2022 />} />
+          <Route path="/tubarao/carnaval2023" element={<Carnaval2023 />} />
+          <Route path="/tubarao/carnaval2024" element={<Carnaval2024 />} />
+          <Route path="/tubarao/carnaval2025" element={<Carnaval2025 />} />
         </Routes>
         <Footer />
       </div>
