45f29b641a9852edaa0331cfdf9d026f5a6d0206
fabriciobrito01 <fabriciodbe@gmail.com>
2025-06-28 21:54:45 -0300

    Rename Carnaval to Enredo

--- frontend/site-tubarao/src/App.jsx
+++ frontend/site-tubarao/src/App.jsx
@@ -2,10 +2,10 @@ import Home from './pages/Home.jsx';
 import InstitutoTubarao from './pages/InstitutoTubarao.jsx';
 import HistoriaTubarao from './pages/HistoriaTubarao.jsx';
 import FichaTecnica from './pages/FichaTecnica.jsx';
-import Carnaval2022 from './pages/Carnaval2022.jsx';
-import Carnaval2023 from './pages/Carnaval2023.jsx';
-import Carnaval2024 from './pages/Carnaval2024.jsx';
-import Carnaval2025 from './pages/Carnaval2025.jsx';
+import Enredo2022 from './pages/Enredo2022.jsx';
+import Enredo2023 from './pages/Enredo2023.jsx';
+import Enredo2024 from './pages/Enredo2024.jsx';
+import Enredo2025 from './pages/Enredo2025.jsx';
 import Navbar from './components/Navbar.jsx';
 import Footer from './components/Footer.jsx'; 
 import './index.css';
@@ -21,10 +21,10 @@ function App() {
           <Route path="/institutotubarao" element={<InstitutoTubarao />} />
           <Route path="/tubarao/historia" element={<HistoriaTubarao />} />
           <Route path="/tubarao/fichatecnica" element={<FichaTecnica />} />
-          <Route path="/tubarao/carnaval2022" element={<Carnaval2022 />} />
-          <Route path="/tubarao/carnaval2023" element={<Carnaval2023 />} />
-          <Route path="/tubarao/carnaval2024" element={<Carnaval2024 />} />
-          <Route path="/tubarao/carnaval2025" element={<Carnaval2025 />} />
+          <Route path="/tubarao/enredo2022" element={<Enredo2022 />} />
+          <Route path="/tubarao/enredo2023" element={<Enredo2023 />} />
+          <Route path="/tubarao/enredo2024" element={<Enredo2024 />} />
+          <Route path="/tubarao/enredo2025" element={<Enredo2025 />} />
         </Routes>
         <Footer />
       </div>
